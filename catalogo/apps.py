56acc5d2b388ca96d319301a24edc88d51f00b55
Joshua A-R <jabrilro@uninpahu.edu.co>
2026-09-18 16:38:34 -0500

    Delete catalogo directory

--- catalogo/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class CatalogoConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "catalogo"
-    # EJEMPLO: puedes poner un nombre más descriptivo, ej. "Inventario"
-    verbose_name = "Catálogo (Modelos 3D)"
